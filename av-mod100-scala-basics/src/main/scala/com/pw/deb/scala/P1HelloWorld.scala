8b89ef35024a2a2474d2616a52a48c4c008ab493
Ashok Vennapusa <ashokvennapusa@Ashoks-MacBook-Pro.local>
2023-02-04 14:10:30 -0800

    feature2: print scala version

--- av-mod100-scala-basics/src/main/scala/com/pw/deb/scala/P1HelloWorld.scala
+++ av-mod100-scala-basics/src/main/scala/com/pw/deb/scala/P1HelloWorld.scala
@@ -2,6 +2,6 @@ package com.pw.deb.scala
 
 object P1HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello world from scala")
+    println("Hello world from scala: " + util.Properties.versionString)
   }
 }
